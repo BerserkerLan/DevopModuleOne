977b5ceb06d61f1a2d85bec867dbcad5c1b9e9a4
Rusab Asher <Rusab.A.Asher@aexp.com>
2020-07-21 13:57:07 +0100

    Added mod operator

--- calculator.py
+++ calculator.py
@@ -14,6 +14,10 @@ with open("step_2.txt", "r") as f:
         elif (input_arr[1] == '/'):
             result = int(input_arr[2]) / int(input_arr[3])
             end_result += result
+        elif (input_arr[1] == '%'):
+            result = int(input_arr[2]) % int(input_arr[3])
+            end_result += result
+
 print("Step2 result ", end_result) # 756216.3057730488
 
 f2 = open("step_3.txt", "r")
@@ -44,4 +48,8 @@ while True:
     elif (input_arr[2] == '/'):
         result = int(int(input_arr[3]) / int(input_arr[4]))
         idx = int(result) - 1
+    elif (input_arr[2] == '%'):
+        result = int(int(input_arr[3]) % int(input_arr[4]))
+        idx = int(result) - 1
+
 print("Step3 IDX: ", idx, " Value ", lines[idx])
